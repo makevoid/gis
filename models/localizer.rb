af6958ea136ac214da35428b7525a1ef6a26d62c
Francesco Canessa <makevoid@gmail.com>
2013-02-14 19:02:14 +0100

    forward

--- models/localizer.rb
+++ models/localizer.rb
@@ -29,14 +29,22 @@ class Localizer
   end
 
   def write_files(name, good, locations, refuseds)
-    File.open("#{PATH}/data/#{name}_good.csv", "w"){ |f| f.write good.join("\n") } unless good.empty?
-    File.open("#{PATH}/data/#{name}_refuseds.csv", "w"){ |f| f.write refuseds.join("\n") } unless refuseds.empty?
+    # File.open("#{PATH}/data/#{name}_good.csv", "w"){ |f| f.write good.join("\n") } unless good.empty?
+    # File.open("#{PATH}/data/#{name}_refuseds.csv", "w"){ |f| f.write refuseds.join("\n") } unless refuseds.empty?
     File.open "#{PATH}/data/#{name}.json", "w" do |file|
       file.write locations.to_json
     end
   end
 
+  COL_SEP = ";"
+
   private
+
+  def log_failure loc_name, project_title
+    map_url = "https://maps.google.com/maps?q=#{loc_name.gsub("\s", "+")}" if loc_name
+    map_url2 = "https://maps.google.com/maps?q=#{project_title.gsub("\s", "+")}" if project_title
+    puts "f> #{loc_name} > #{map_url}\n#{project_title} > #{map_url2}\n"
+  end
 
   def localize_name(name)
     locations = []
@@ -44,28 +52,36 @@ class Localizer
     good = []
     puts "geocoding #{name}:\nfailures:"
     idx = 0
-    CSV.foreach("#{PATH}/data/#{name}.csv", { col_sep: ";" }) do |row|
+    CSV.foreach("#{PATH}/data/#{name}.csv", { col_sep: COL_SEP }) do |row|
       idx += 1
       next if idx == 1
 
       loc_name, category, project_id, cris_id, project_title = row
+      geo_field = "name"
       geo = Geocoder.search(loc_name).first
-      if geo
-        row += [project_title, geo.latitude, geo.longitude]
-        good << row.join(";")
-        locations << { name: loc_name, category: category, lat: geo.latitude, lng: geo.longitude, project_id: project_id, cris_id: cris_id, project_title: project_title }
-      else
-        refuseds << row.join(";")
-        map_url = "https://maps.google.com/maps?q=#{loc_name.gsub("\s", "+")}" if loc_name
-        puts "f> #{loc_name} > #{map_url}"
-      end
 
+      location = { name: loc_name, category: category, project_id: project_id, cris_id: cris_id, project_title: project_title, geo_field: geo_field }
+
+      if geo
+        # row += [project_title, geo.latitude, geo.longitude]
+        # good << row.join(COL_SEP)
+        locations << location.merge( lat: geo.latitude, lng: geo.longitude )
+      else
+        geo2 = Geocoder.search(project_title).first
+        if geo2
+          locations << location.merge( lat: geo.latitude, lng: geo.longitude )
+        else
+          refuseds << row.join(COL_SEP)
+          log_failure loc_name, project_title
+        end
+      end
 
       sleep 0.5
     end
 
     write_files name, good,  locations, refuseds
 
-    puts "\n\nfinished - real geocoding api: http://code.google.com/apis/ajax/playground/#geocoding_simple"
+    puts "\n\nfinished!\ngood: #{locations.size}, refuseds: #{refuseds.size}"
+    # real geocoding api: http://code.google.com/apis/ajax/playground/#geocoding_simple
   end
 end
